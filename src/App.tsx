5f908965d200da71ba6231cc6ad2acb256b6e8d3
Bret Funk <bret.e.funk@gmail.com>
2019-09-27 09:41:22 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+
 
 const App: React.FC = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The count is: {count}.</h1>
+      <button onClick={() => setCount(count + 1)}>Plus one</button>
+      <button onClick={() => setCount(count - 1)}>Minus one</button>
     </div>
   );
-}
+};
 
 export default App;
